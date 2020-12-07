709ffded360b5226aef0e2a070029afc351f6d69
Elton Costa <eltonbrasilcosta@gmail.com>
2020-12-06 20:31:59 -0500

    Xception neural network basic files

---
+++ src/xception/cnn/main.py
@@ -0,0 +1,11 @@
+# ----------------------------------------------------------------------------
+# Import library file
+
+from training import *
+from function import *
+
+print ("[INFO] TRAINING IS DONE!")
+print("\n")
+
+plot_acc_loss(result, EPOCHS)
+print("\n")
