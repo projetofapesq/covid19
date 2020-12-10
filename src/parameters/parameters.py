a292775438e25fb14dc7a9057b186719bdeeef53
Elton Costa <eltonbrasilcosta@gmail.com>
2020-12-09 22:25:14 -0500

    A dedicated directory only for the parameters setup

---
+++ src/parameters/parameters.py
@@ -0,0 +1,11 @@
+# ----------------------------------------------------------------------------
+# Parameters for Training and Validation
+
+DATASET_PATH  = '/Users/elton/diagnosis/data/train'
+TEST_DIR      = '/Users/elton/diagnosis/data/test'
+
+IMAGE_SIZE    = (150, 150)
+BATCH         = 10
+EPOCHS        = 100
+DECAY         = 1e-6
+LEARNING_RATE = 0.0001
